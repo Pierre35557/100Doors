ae54b5c51fa2bb98b0ce450a71b485bd73baa310
Pierre <Pierre@warpdevelopment.com>
2020-04-09 13:22:09 +0200

    Initial Commit

---
+++ 100Doors/100Doors/100DoorsTest.cs
@@ -0,0 +1,148 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace _100Doors
+{
+    [TestFixture]
+    public class _100DoorsTest
+    {
+        [Test]
+        public void Given100Doors_And1stPass_100DoorsShouldBeOpen()
+        {
+            //Arrange
+            var round = 1;
+
+            var sut = new Doors();
+
+            //Act
+            var actual = sut.Open(round);
+            var expected = "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@";
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Given100Doors_And2ndPass_50DoorsShouldBeOpen()
+        {
+            //Arrange
+            var round = 2;
+
+            var sut = new Doors();
+
+            //Act
+            var actual = sut.Open(round);
+            var expected = "@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#";
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Given100Doors_AndThirdPass_49DoorsShouldBeOpen()
+        {
+            //Arrange
+            var round = 3;
+
+            var sut = new Doors();
+
+            //Act
+            var actual = sut.Open(round);
+            var expected = "@###@@@###@@@###@@@###@@@###@@@###@@@###@@@###@@@###@@@###@@@###@@@###@@@###@@@###@@@###@@@###@@@###";
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Given100Doors_AndFourthPass_58DoorsShouldBeOpen()
+        {
+            //Arrange
+            var round = 4;
+
+            var sut = new Doors();
+
+            //Act
+            var actual = sut.Open(round);
+            var expected = "@##@@@@@##@#@##@@@@@##@#@##@@@@@##@#@##@@@@@##@#@##@@@@@##@#@##@@@@@##@#@##@@@@@##@#@##@@@@@##@#@##@";
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Given100Doors_AndFifthPass_52DoorsShouldBeOpen()
+        {
+            //Arrange
+            var round = 5;
+
+            var sut = new Doors();
+
+            //Act
+            var actual = sut.Open(round);
+            var expected = "@##@#@@@#@@#@#@@@@@###@####@@#@@####@###@@@@@#@#@@#@@@#@##@@@##@#@@@#@@#@#@@@@@###@####@@#@@####@###";
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Given100Doors_AndSixthPass_54DoorsShouldBeOpen()
+        {
+            //Arrange
+            var round = 6;
+
+            var sut = new Doors();
+
+            //Act
+            var actual = sut.Open(round);
+            var expected = "@##@##@@#@@@@#@@@#@###@@###@@@@@###@@###@#@@@#@@@@#@@##@##@#@##@##@@#@@@@#@@@#@###@@###@@@@@###@@###";
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Given100Doors_AndHundredthPass_ShouldOpen10Doors()
+        {
+            //Arrange
+            var round = 100;
+
+            var sut = new Doors();
+
+            //Act
+            var actual = sut.Open(round);
+            var expected = "@##@####@######@########@##########@############@##############@################@##################@";
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Doors
+    {
+        public string Open(int round)
+        {
+            var doors = new List<string>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                doors.Add("@");
+            }
+
+            //Start looping at second round. First round will always return all open doors.
+            for (int i = 2; i <= round; i++)
+            {
+                for (int j = 1; j <= 100; j++)
+                {
+                    if (j % i == 0)
+                    {
+                        doors[j - 1] = doors[j - 1] == "@" ? "#" : "@";
+                    }
+                }
+            }
+
+            return string.Join("", doors);
+        }
+    }
+}
---
+++ 100Doors/100Doors/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("100Doors")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("100Doors")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5727c08c-ab19-4b13-b7c4-7b2786345144")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
